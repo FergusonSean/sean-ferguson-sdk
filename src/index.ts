7ef3e2d2e4fd160bc2110bd7a5682ec2f313c1ab
Sean Ferguson <sean.ferguson.sf@gmail.com>
2023-03-31 09:32:59 -0700

    Fix exponential backoff

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,8 @@ export class Client {
   }
 
   async sendRequest(path: string, query = '') {
-    for(let retries = 10; retries > 0; retries -= 1) {
+    for(let retries = 10; retries >= 0; retries -= 1) {
+      console.log('trying', path, retries)
       try {
         const response = await axios.get<{docs: [Record<string, any>]}>(
           `${this.baseUrl}${path}${query ? `?${query}` : ''}`, 
@@ -69,15 +70,17 @@ export class Client {
             }
           }
         )
+        console.log(response.data)
         return response?.data?.docs
       } catch (e) {
-        if((e as AxiosError)?.response?.status === 429 && retries > 0) {
+        if((e as AxiosError)?.response?.status === 429) {
           await new Promise(res => {
             setTimeout(res, 2**(11 - retries) * 1000)
           })
+          continue;
         }
 
-        console.log(e)
+        console.log(e.response.status)
         throw e
       } 
     }
