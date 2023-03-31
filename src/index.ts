838df5ce2929b028a3add5d4d2aa746dff6d797c
Sean Ferguson <sean.ferguson.sf@gmail.com>
2023-03-31 11:23:51 -0700

    Lint fixes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import axios from 'axios';
 import type {AxiosError} from 'axios';
 
-type FilterEntries<KeyType extends (string | number | symbol) = string> = Record<KeyType, Filter>;
-
-type Filter = VerboseFilter | AliasFilter;
-
-type AliasFilter = boolean | VerboseFilter["value"];
+export enum OPS {
+  EXISTS = '',
+  NOT_EXISTS = '!',
+  GT = '>',
+  GTE = '>=',
+  LT = '<',
+  LTE = '<=',
+  MATCH = '=',
+  NOT_MATCH = '!=',
+}
 
 type VerboseFilter = { 
   op: OPS, 
   value?: { toString: () => string} | [{ toString: () => string}]
 };
+
+type AliasFilter = boolean | VerboseFilter["value"];
+
+type Filter = VerboseFilter | AliasFilter;
+
+type FilterEntries<KeyType extends (string | number | symbol) = string> = Record<KeyType, Filter>;
 
 interface RequestParams {
   limit?: number;
@@ -19,7 +30,9 @@ interface RequestParams {
   sort?: string;
 }
 
-interface FilterableRequestParams<FilterableFieldType extends (string | number | symbol) = string> extends RequestParams {
+interface FilterableRequestParams<
+  FilterableFieldType extends (string | number | symbol) = string
+> extends RequestParams {
   filters?: FilterEntries<FilterableFieldType>;
 }
 
@@ -42,104 +55,99 @@ interface Quote {
   id: string;
 }
 
-export enum OPS {
-  EXISTS = '',
-    NOT_EXISTS = '!',
-    GT = '>',
-    GTE = '>=',
-    LT = '<',
-    LTE = '<=',
-    MATCH = '=',
-    NOT_MATCH = '!=',
-}
 
-  export class Client {
-    token: string;
+export class Client {
+  token: string;
 
-    baseUrl: string;
+  baseUrl: string;
 
-    constructor({token, baseUrl = 'https://the-one-api.dev/v2' }: {token: string, baseUrl?: string }) {
-      this.token = token;
-      this.baseUrl = baseUrl
-    }
-
-    static generateQuery<FilterKeyType extends (string | number | symbol)>(params: FilterableRequestParams<FilterKeyType>) {
-      const {filters, ...paginationFields} = params;
-      let query = Object.entries(paginationFields)
-        .map(([k, v]: [string,string]) => `${k}=${v}`)
-        .join('&')
-
-      if(filters) {
-        query += Object.entries(filters).map(f => {
-          const [field, rawFilter] = f as [string, Filter]
-
-          let filter: VerboseFilter;
-
-          if(rawFilter === true) {
-            filter = { op: OPS.EXISTS }
-          } else if(rawFilter === false) {
-            filter = { op: OPS.NOT_EXISTS }
-          } else if((rawFilter as VerboseFilter)?.op) {
-            filter = rawFilter as VerboseFilter
-          } else {
-            filter = { op: OPS.MATCH, value: rawFilter}
-          }
-
-          switch(filter.op) {
-            case OPS.EXISTS:
-              case OPS.NOT_EXISTS:
-              return `${filter.op}${field}`
-            default:
-              return `${field}${filter.op}${[filter.value].flat().join(',')}`
-          }
-
-        }).join('&')
-      }
-
-      return query;
-
-    }
-
-    async sendRequest<ResponseType>(path: string, query = ''): Promise<[ResponseType]> {
-      for(let retries = 10; retries >= 0; retries -= 1) {
-        try {
-          const response = await axios.get<{docs: [ResponseType]}>(
-            `${this.baseUrl}${path}${query ? `?${query}` : ''}`, 
-            {
-              headers: {
-                'Authorization': `Bearer ${this.token}`
-              }
-            }
-          )
-          return response?.data?.docs
-        } catch (e) {
-          if((e as AxiosError)?.response?.status === 429) {
-            await new Promise(res => {
-              setTimeout(res, 2**(11 - retries) * 1000)
-            })
-            continue;
-          }
-
-          throw e
-        } 
-      }
-
-      return [] as unknown as [ResponseType];
-
-    }
-
-    async getMovie(movieId: string): Promise<Movie> {
-      const movies = await this.sendRequest<Movie>(`/movie/${movieId}`);
-      return movies[0]
-    }
-
-    getMovieQuotes(movieId: string, params: RequestParams = {}): Promise<[Quote]> {
-      return this.sendRequest<Quote>(`/movie/${movieId}/quote`, Client.generateQuery(params))
-    }
-
-    async getMovies(params: FilterableRequestParams<keyof Movie> = {}): Promise<[Movie]> {
-      const query = Client.generateQuery<keyof Movie>(params)
-
-      return this.sendRequest<Movie>('/movie', query)
-    }
+  constructor({
+    token, 
+    baseUrl = 'https://the-one-api.dev/v2'
+  }: {token: string, baseUrl?: string }) {
+    this.token = token;
+    this.baseUrl = baseUrl
   }
+
+  static generateQuery<FilterKeyType extends (string | number | symbol)>(
+    params: FilterableRequestParams<FilterKeyType>
+  ) {
+    const {filters, ...paginationFields} = params;
+    let query = Object.entries(paginationFields)
+    .map(([k, v]: [string,string]) => `${k}=${v}`)
+    .join('&')
+
+    if(filters) {
+      query += Object.entries(filters).map(f => {
+        const [field, rawFilter] = f as [string, Filter]
+
+        let filter: VerboseFilter;
+
+        if(rawFilter === true) {
+          filter = { op: OPS.EXISTS }
+        } else if(rawFilter === false) {
+          filter = { op: OPS.NOT_EXISTS }
+        } else if((rawFilter as VerboseFilter)?.op) {
+          filter = rawFilter as VerboseFilter
+        } else {
+          filter = { op: OPS.MATCH, value: rawFilter}
+        }
+
+        switch(filter.op) {
+          case OPS.EXISTS:
+            case OPS.NOT_EXISTS:
+            return `${filter.op}${field}`
+          default:
+            return `${field}${filter.op}${[filter.value].flat().join(',')}`
+        }
+
+      }).join('&')
+    }
+
+    return query;
+
+  }
+
+  async sendRequest<ResponseType>(path: string, query = ''): Promise<[ResponseType]> {
+    for(let retries = 10; retries >= 0; retries -= 1) {
+      try {
+        const response = await axios.get<{docs: [ResponseType]}>(
+          `${this.baseUrl}${path}${query ? `?${query}` : ''}`, 
+          {
+            headers: {
+              'Authorization': `Bearer ${this.token}`
+            }
+          }
+        )
+        return response?.data?.docs
+      } catch (e) {
+        if((e as AxiosError)?.response?.status === 429) {
+          await new Promise(res => {
+            setTimeout(res, 2**(11 - retries) * 1000)
+          })
+          continue;
+        }
+
+        throw e
+      } 
+    }
+
+    return [] as unknown as [ResponseType];
+
+  }
+
+  async getMovie(movieId: string): Promise<Movie> {
+    const movies = await this.sendRequest<Movie>(`/movie/${movieId}`);
+    return movies[0]
+  }
+
+  getMovieQuotes(movieId: string, params: RequestParams = {}): Promise<[Quote]> {
+    return this.sendRequest<Quote>(`/movie/${movieId}/quote`, Client.generateQuery(params))
+  }
+
+  async getMovies(params: FilterableRequestParams<keyof Movie> = {}): Promise<[Movie]> {
+    const query = Client.generateQuery<keyof Movie>(params)
+
+    return this.sendRequest<Movie>('/movie', query)
+  }
+}
